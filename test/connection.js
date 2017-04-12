e6b5c72f4e7fed458a85a1f7a454e19ba9e94adf
JMSobrinos <libreyextremo@gmail.com>
2017-04-12 12:33:10 +0200

    implementing before function

--- test/connection.js
+++ test/connection.js
@@ -3,14 +3,18 @@ const mongoose = require('mongoose');
 // use ES6 promises library instead of mongoose promise library
 mongoose.Promise = global.Promise;
 
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/simpleclientdb');
+// It should connect to the database before tests run
+before(function(done){
 
-// launch when connection is opened
-console.log('Launch connection.js');
-mongoose.connection.once('open', function(){
-  console.log('MongoDB connection has been made succesfully.')
-}).on('error', function(error){
-  console.log('MongoDB connection error:', error);
+  // Connect to mongodb
+  mongoose.connect('mongodb://localhost/simpleclientdb');
+  // launch when connection is opened
+  console.log('Launch connection.js');
+  mongoose.connection.once('open', function(){
+    console.log('MongoDB connection has been made succesfully.');
+    done();
+  }).on('error', function(error){
+    console.log('MongoDB connection error:', error);
+  });
+
 });
-console.log('End connection.js');
