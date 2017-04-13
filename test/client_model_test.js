18d58d9a2451bbf1bd61264b33a19cbdc49c00da
JMSobrinos <libreyextremo@gmail.com>
2017-04-13 12:33:15 +0200

    Deleting tests added

--- test/client_model_test.js
+++ test/client_model_test.js
@@ -7,6 +7,7 @@ describe('Client collection tests', function(){
 
   var clientRecord1;
   var clientRecord2;
+  var clientRecord3;
   var clientRecord;
 
   // actions executes before each test
@@ -100,11 +101,55 @@ describe('Client collection tests', function(){
       ]
     });
 
+    clientRecord3 = new ClientModel({
+      company_nif: 'R23699364',
+      company_name: 'Aquarium S.L.',
+      company_type: 'Professional services',
+      company_num_employees: 6,
+      company_balance: 45000,
+      company_cnae: 3014,
+      company_address: 'S/N Principe Avenue',
+      company_phone: '+34925892300',
+      company_city: 'Talavera',
+      company_state: 'Toledo',
+      company_postal_code: 45600,
+      company_country: 'Spain',
+      company_email: 'aquarium.contact@aquariumsl.com',
+      company_year_of_creation: 2014,
+      company_isActive: true,
+      company_picture: '',
+      company_member: [
+        {
+          member_position: 'President',
+          member_name: 'Gonzo Duran',
+          member_info: '',
+          member_phone: ''
+        },
+        {
+          member_position: 'VicePresident',
+          member_name: 'Emilio Duran',
+          member_info: '',
+          member_phone: ''
+        }
+      ],
+      company_bill: [
+        {
+          bill_id: 201702010001,
+          bill_date: '02/01/2017',
+          bill_concept: 'Logotype design',
+          bill_total: 150
+        }
+      ]
+    });
+
     clientRecord1.save().then(function() {
       assert(clientRecord1.isNew === false);
       clientRecord2.save().then(function() {
         assert(clientRecord2.isNew === false);
-        done();
+        clientRecord3.save().then(function() {
+          assert(clientRecord3.isNew === false);
+          done();
+        });
       });
     });
 
@@ -188,7 +233,7 @@ describe('Client collection tests', function(){
 
   });
 
-  // finding a record in client collection
+  // finding a record using a var in client collection
   it('Finding a record using a var from client collection', function(done){
 
     ClientModel.findOne({ company_name: clientRecord2.company_name }).then(function(result){
@@ -198,13 +243,62 @@ describe('Client collection tests', function(){
 
   });
 
-
-  // finding a record by IDin client collection
+  // finding a record by ID in client collection
   it('Finding a record by ID from client collection', function(done){
 
     ClientModel.findOne({ _id: clientRecord2._id }).then(function(result){
       assert(result._id.toString() === clientRecord2._id.toString() );
       done();
+    });
+
+  });
+
+  // deleting a record in client collection
+  it('Deleting a record from client collection', function(done){
+
+    ClientModel.findOneAndRemove({ company_name: 'Aquarium S.L.' }).then(function(){
+      ClientModel.findOne({ company_name: 'Aquarium S.L.'}).then(function(result){
+        assert(result === null);
+        done();
+      });
+    });
+
+  });
+
+  // deleting a record by ID in client collection
+  it('Deleting a record by ID from client collection', function(done){
+
+    ClientModel.findOneAndRemove({ _id: clientRecord2._id }).then(function(){
+      ClientModel.findOne({ _id: clientRecord2._id}).then(function(result){
+        assert(result === null);
+        done();
+      });
+    });
+
+  });
+
+  // deleting a group of records in client collection
+  it('Deleting a group of records from client collection', function(done){
+
+    ClientModel.remove({ company_postal_code: 45600 }).then(function(){
+      ClientModel.findOne({ company_postal_code: 45600 }).then(function(result){
+        assert(result === null)
+        done();
+      });
+    });
+
+  });
+
+  // deleting a record by var in client collection
+  it('Deleting a record by var from client collection', function(done){
+    var nametemp = clientRecord2.company_name;
+    // if you want to check value:
+    //console.log('nametemp: ' + nametemp);
+    clientRecord2.remove().then(function(){
+      ClientModel.findOne({ company_name: nametemp }).then(function(result){
+        assert(result === null)
+        done();
+      });
     });
 
   });
