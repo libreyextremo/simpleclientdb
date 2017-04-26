fa15693b0e37a57235df3b4894186348037b39be
JMSobrinos <libreyextremo@gmail.com>
2017-04-26 12:38:22 +0200

    add api to the route

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // require express so we can use it
 const express = require('express');
-const routes = require('./routes/api');
 
 // create and set up express app
 const app = express();
 
-// use routes defined in files
-app.use(routes);
+// use routes defined in files adding '/api' to the route
+// 1st param: additional route (localhost:4000/XXXXX/)
+// 2nd param: location of file api.js (/routes/api.js file)
+app.use('/api', require('./routes/api'));
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
